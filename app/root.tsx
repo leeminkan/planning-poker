540589c7841166b2ee665d6e056893be6a4869b0
leeminkan <nguyenhoangphuc.kan@gmail.com>
2024-09-11 21:59:45 +0700

    feat: init frontend with shadcn

---
+++ app/root.tsx
@@ -0,0 +1,36 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+
+import styles from "./tailwind.css?url";
+import { Button } from "./components/ui/button";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <Button>Test</Button>
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return <Outlet />;
+}
